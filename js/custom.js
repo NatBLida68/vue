fd578ea5d43a0e405bdea6aa32bc207aaf32e454
ashwa <aswajith@glaubetech.com>
2021-08-26 12:11:13 +0530

    so far its okei

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,22 @@
 let app = new Vue({
-    // all options goes here
-})
+    el: "#app",    //el = element app = id'
+    data: {
+        alert: "This is an alert message! ",
+        titleHTML : "<span class='badge'>Baby Dragon</span>",
+        btn : "<button class='btn btn-default'>baby dragon</button>",
+        projects: [
+            {title: "portfolio", languages: ["HTML", "CSS", "VueJS"]},
+        ],
+        dynamicId : "projects_section",
+        dynamicClass : "bg-primary",
+        //events
+        event_alpha: "click"
+    },
+    methods: {
+        eventAlpha() {
+            console.log("test click function");
+        }
+    }
+});
+console.log(app.alert)
+
